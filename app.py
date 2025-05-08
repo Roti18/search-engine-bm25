8750f2303bf7d39ccef6fc41a1965577e5afc396
Roti18 <akuronif@gmail.com>
2025-05-08 09:14:51 +0700

    add static

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-  return "Hello indonesia"
+def tambah(a, b):
+    return a + b
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    hasil = None
+    if request.method == 'POST':
+        angka1 = int(request.form['angka1'])
+        angka2 = int(request.form['angka2'])
+        hasil = tambah(angka1, angka2)
+    return render_template('index.html', hasil=hasil)
+
 
 if __name__ == "__main__":
   app.run(debug=True)
