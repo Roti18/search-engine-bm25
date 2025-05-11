a76d4e714e879a759b1c30e82e30f96abf7d3c4e
Roti18 <akuronif@gmail.com>
2025-05-11 11:04:51 +0700

    add method append, any, all, map, filter, enumerate & zip

--- logic.py
+++ logic.py
@@ -1,15 +1,9 @@
-  # sorted() – Jika butuh urutkan elemen (misal, urutkan angka atau string).
-  # any() dan all() – Berguna untuk mengecek kondisi di dalam list atau koleksi.
-  # map() dan filter() – Berguna untuk transformasi atau filtering koleksi.
-  # enumerate() – Berguna ketika kamu perlu mengakses indeks dan nilai sekaligus.
-  # zip() – Untuk menggabungkan dua atau lebih list secara paralel, sering dipakai di tugas-tugas data.
-
-# any() → has_any
-# all() → has_all
-# map() → apply
-# filter() → select
-# enumerate() → index
-# zip() → combine
+# append(gabungkan isi)
+# list = []
+# item = apa yang ingin di gabung
+def gabung(list, item):
+  list += [item]
+  return list
 
 # len(membaca berapa elemen yang ada di suatu list)
 def semua(n):
@@ -17,7 +11,7 @@ def semua(n):
   for i in n:
     count += 1
   return count
-  # print(semua("testing hallo"))
+print(semua("testing hallo"))
 
 # sum(menghitung semua elemen angka)
 def count(n):
@@ -58,3 +52,66 @@ def sort(n):
             i += 1
     return n
 # print(sort([19,8,3]))
+
+# any(Mengembalikan True jika minimal satu elemen dalam iterable bernilai True.)
+def ada(n):
+  for i in n:
+    if i:
+      return True
+  return False
+# print(ada([0,0,0]))
+
+# all(Mengembalikan True hanya jika semua elemen dalam iterable bernilai True.)
+def allTrue(n):
+  for i in n:
+    if not i:
+      return False
+  return True
+# print(allTrue([0,1,1,1]))
+
+# map(Menerapkan fungsi ke setiap elemen dalam iterasi.)
+def applyAll(func, n):
+    return [func(i) for i in n]
+# angka = [1, 2, 3]
+# hasil = applyAll(lambda x: x * 2, angka)
+# print(hasil)
+
+
+# filter(Menyaring elemen dalam iterable berdasarkan kondisi fungsi yang diberikan (mengembalikan True))
+def saring(func, n):
+    hasil = []
+    for i in n:
+        if func(i):
+            gabung(hasil, i)
+    return hasil
+# angka = [1, 2, 3, 4, 5]
+# genap = saring(lambda x: x % 2 == 0, angka)
+# print(genap)
+
+
+# enumerate(Memberikan indeks dan nilai dari iterasi)
+def index(n):
+  i = 0
+  result = []
+  for item in n:
+    gabung(result,(i, item))
+    i+=1
+  return result
+# buah = ['apel', 'jeruk', 'mangga']
+# for i, nama in index(buah):
+#     print(i, nama)
+
+
+# zip(Menggabungkan dua atau lebih iterable secara berpasangan (sejajar))
+def collabLoop(func, n):
+    result = []
+    i = 0
+    while i < semua(func):
+      while i < semua(n):
+        result.append((func[i], n[i]))
+        i += 1
+    return result
+# nama = ['Ani', 'Budi']
+# nilai = [80, 90]
+# gabung = collabLoop(nama, nilai)
+# print(gabung)
